33432966a58bb9c6723f621a3c38d74563facf59
broman1233 <49570737+broman1233@users.noreply.github.com>
2019-04-12 15:34:08 -0400

    Add files via upload

---
+++ Lab4BankProgram.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+public class Lab4BankProgram {
+
+	public static void main(String[] args) 
+	{
+		int programOption = -1 ;
+		
+		startMsg();
+		Scanner option = new Scanner(System.in);
+		
+		do
+		{
+			programOption = option.nextInt();
+			switch (programOption)
+		    {
+		        case 1:
+		            System.out.println("");
+		            break;
+		        case 2: 
+		            // ..something else
+		            break;
+		        case 3: 
+		            // .. exit program
+		            break;
+		        case 4: 
+		            // .. exit program
+		            break;
+		        case 5: 
+		            // .. exit program
+		            break;
+		            
+		        default:
+		            System.out.println("Choice must be a value between 1 and 5.");
+		    }   
+			
+			
+			
+			
+			
+
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}while(programOption != 5);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+	static void startMsg()
+	{
+		System.out.println("Hi welcome to your banking program!");
+		System.out.println("Select the number next to the action to choose that option.");
+		System.out.println("1. Make a Bank Account.");
+		System.out.println("2. Make a Deposit.");
+		System.out.println("3. Make a Withdrawl.");
+		System.out.println("4. Make a Transfer.");
+		System.out.println("5. Exit.");
+	}
+	
+	
+
+}
